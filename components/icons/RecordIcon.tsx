9af7a5ba55aedb767d6acbddb825338a52c375c0
Success009 <successadhikari1234@gmail.com>
2025-07-11 19:06:42 +0545

    Replace all files with new project version

---
+++ components/icons/RecordIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const RecordIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.536 8.464a5 5 0 010 7.072m-7.072-7.072a5 5 0 000 7.072m7.072-7.072l-7.072 7.072" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 18.5a6.5 6.5 0 100-13 6.5 6.5 0 000 13z"/>
+    </svg>
+);
